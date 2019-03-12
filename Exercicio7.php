fccb96f7d4da556d56c84d9b21d34841f0c5365a
Gutoovelha <48135676+Gutoovelha@users.noreply.github.com>
2019-03-12 16:30:59 -0300

    Add files via upload

---
+++ Exercicio7.php
@@ -0,0 +1,16 @@
+<?php 
+
+  print "Quantos km você andou com o carro?";
+  $km_rodados = fgets (STDIN);
+  print "\n";
+
+  print "Quantos dias você andou com o carro?";
+  $dias_rodados = fgets (STDIN);
+  print "\n";
+
+  $total =(($dias_rodados*60)+$km_rodados*0.15);
+
+  print "Você irá gastar R$$total";
+  print "\n\n";
+
+ 
