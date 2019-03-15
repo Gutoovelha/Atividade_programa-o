327b3c3a9b789fdc532ccd827f508c542470c504
Guto <48135676+Gutoovelha@users.noreply.github.com>
2019-03-15 16:31:28 -0300

    Update Exercicio7.php

--- Exercicio7.php
+++ Exercicio7.php
@@ -1,16 +1,10 @@
 <?php 
-
-  print "Quantos km você andou com o carro?";
-  $km_rodados = fgets (STDIN);
+  print "Coloque os km que você andou com o carro:\n";
+  $km_rodados = (int)fgets (STDIN);
   print "\n";
-
-  print "Quantos dias você andou com o carro?";
-  $dias_rodados = fgets (STDIN);
+  print "Coloque os dias que você andou com o carro:\n";
+  $dias_rodados = (int)fgets (STDIN);
   print "\n";
-
   $total =(($dias_rodados*60)+$km_rodados*0.15);
-
   print "Você irá gastar R$$total,00";
   print "\n\n";
-
- 
